6e3dd1b0cc5c125c70f7554dc9769b6cb0e1527f
cintiaMillacura2207 <cg2207m@gmail.com>
2022-12-08 01:01:30 -0300

    Se agrego la pantalla

--- calculadora.py
+++ calculadora.py
@@ -10,4 +10,9 @@ root.title("Calculadora")
 marco=Frame(root,bg="violet",width="650",height="350")
 marco.pack(fill="none",expand="True")
 
+pantalla=Entry(marco)
+pantalla.grid(row=0,columnspan=5,sticky=W+E,pady=5,padx=3)
+pantalla.config(bg="lightblue",fg="black",width=30,font=("Arial",18,'bold'),borderwidth=2,relief="solid",
+               justify=RIGHT)
+
 root.mainloop()
